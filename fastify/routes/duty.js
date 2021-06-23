eefbec9aeb41b2db35c5b369f1731f5463252c9f
Anton Babentsov <ababentsov@gmail.com>
2021-06-23 19:08:20 +0200

    Couple more GET requests for /duty

--- fastify/routes/duty.js
+++ fastify/routes/duty.js
@@ -14,6 +14,40 @@ async function routes (fastify, options) {
         
         res.send(list)
     })
-  }
+
+    fastify.get('/duty/id/:dutyId', async(req, res) => {
+        // try it without parceInt to see what happens
+        //var dutyId = (req.params.dutyId)
+        var dutyId = parseInt(req.params.dutyId)
+
+        const list = await duty.findMany({
+            select: {
+                id: true,
+                task: true,
+                stuff: true
+            },
+            where: {
+                id: dutyId
+            }
+        })
+        res.send(list)
+    })
+
+    fastify.get('/duty/task/:taskName', async(req, res) => {
+        var taskName = req.params.taskName
+
+        const list = await duty.findMany({
+            select: {
+                id: true,
+                task: true,
+                stuff: true
+            },
+            where: {
+               task : taskName
+            }
+        })
+        res.send(list)
+    })
+}
     
-  module.exports = routes
+module.exports = routes
